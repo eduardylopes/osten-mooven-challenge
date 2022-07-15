fd8e9cf3f8e67eb8ffd57242c1d30e20e88d4869
Eduardy <eduardylopes@gmail.com>
2022-07-15 18:29:34 -0300

    create company module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CompanyModule],
 })
 export class AppModule {}
