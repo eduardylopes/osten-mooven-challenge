cb348f45e3299e59f367cad771872bd6e0c31768
Eduardy <eduardylopes@gmail.com>
2022-07-15 23:17:43 -0300

    create validation pipes

--- back-end/src/company/dtos/create-company.dto.ts
+++ back-end/src/company/dtos/create-company.dto.ts
@@ -1,18 +1,50 @@
-interface CompanyAddress {
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNumber,
+  IsString,
+  Matches,
+  ValidateNested,
+} from 'class-validator';
+
+export class CompanyAddress {
+  @IsString()
   street: string;
+
+  @IsNumber()
   number: number;
+
+  @IsString()
   complement: string;
+
+  @IsString()
   district: string;
+
+  @IsString()
   city: string;
+
+  @IsString()
   state: string;
+
+  @IsString()
   telephone: string;
 }
 
-interface CreateCompanyDto {
+export class CreateCompanyDto {
+  @IsString()
   corporate_name: string;
+
+  @IsString()
   name: string;
+
+  @Matches(/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/, {
+    message: 'cnpj must be valid',
+  })
+  @IsString()
   cnpj: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CompanyAddress)
   address: CompanyAddress[];
 }
-
-export { CreateCompanyDto };
