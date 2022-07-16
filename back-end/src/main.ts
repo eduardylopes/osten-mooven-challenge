f204a82b693c1b34986b53c9c044b717b51797ca
Eduardy <eduardylopes@gmail.com>
2022-07-16 02:22:47 -0300

    implement swagger api doc

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Osten Moove')
+    .setDescription('The company API description')
+    .setVersion('1.0')
+    .addTag('Company')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
