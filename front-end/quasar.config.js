f1c1b853c722221385db3688db1877c6df2f4df3
Eduardy <eduardylopes@gmail.com>
2022-07-17 22:46:22 -0300

    add vercel file

--- front-end/quasar.config.js
+++ front-end/quasar.config.js
@@ -19,6 +19,8 @@ module.exports = configure(function () {
         node: "node16",
       },
 
+      distDir: ctx.mode.spa ? "public" : null,
+
       env: {
         API: "http://localhost:3000/api",
       },
