03598dc43555ee844be8eefb644770c82c99205c
Eduardy <eduardylopes@gmail.com>
2022-07-17 23:06:59 -0300

    fix backend port

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
